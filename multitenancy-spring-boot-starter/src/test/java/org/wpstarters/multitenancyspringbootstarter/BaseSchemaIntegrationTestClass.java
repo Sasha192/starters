2b2064f8206a828e7e73996cc9b837eb1e8cde96
C5323037 <oleksandr.bunin@sap.com>
2021-12-05 18:52:47 +0200

    schema tests

--- multitenancy-spring-boot-starter/src/test/java/org/wpstarters/multitenancyspringbootstarter/BaseSchemaIntegrationTestClass.java
+++ multitenancy-spring-boot-starter/src/test/java/org/wpstarters/multitenancyspringbootstarter/BaseSchemaIntegrationTestClass.java
@@ -12,8 +12,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.sql.SQLException;
-
 @SpringJUnitWebConfig
 @SpringBootTest(classes = {SchemaTestConfiguration.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @ActiveProfiles("test_schema")
