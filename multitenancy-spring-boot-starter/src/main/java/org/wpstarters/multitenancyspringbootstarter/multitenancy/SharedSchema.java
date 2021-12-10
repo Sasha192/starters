367648c6647d0aa287325c7dae7e16aaee5d9f77
C5323037 <oleksandr.bunin@sap.com>
2021-12-10 19:54:02 +0200

    removed multitenancy spring boot

--- multitenancy-spring-boot-starter/src/main/java/org/wpstarters/multitenancyspringbootstarter/multitenancy/SharedSchema.java
+++
@@ -1,9 +0,0 @@
-package org.wpstarters.multitenancyspringbootstarter.multitenancy;
-
-public class SharedSchema extends MultitenancyConditional {
-
-    @Override
-    protected Multitenancy getMultitenancy() {
-        return Multitenancy.SHARED_SCHEMA;
-    }
-}
--- multitenancy-spring-boot-starter/src/main/java/org/wpstarters/multitenancyspringbootstarter/multitenancy/exceptions/TenantCreationException.java
+++
@@ -1,7 +0,0 @@
-package org.wpstarters.multitenancyspringbootstarter.multitenancy.exceptions;
-
-public class TenantCreationException extends RuntimeException {
-    public TenantCreationException(String message, Throwable e) {
-        super(message, e);
-    }
-}
